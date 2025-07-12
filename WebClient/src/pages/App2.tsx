f54219325efad93d249d189a4aa5a785a4a5c4e6
Hector N <>
2025-07-12 15:27:52 +0700

    Update show video client

--- WebClient/src/pages/App2.tsx
+++ WebClient/src/pages/App2.tsx
@@ -22,13 +22,7 @@ declare global {
 
 const App2 = (props) => {
   const { handleVideoYoutubeId, handleCurrentPart, handleSeconds, handleMute, currentPart,
-    handleTriggerPlay,
-    videoYoutubeId,
-    triggerPlay,
-    seconds,
-    isReady,
-    handleStateChange,
-    slugHomepage } = props;
+    videoYoutubeId, triggerPlay, seconds, isReady, handleStateChange, slugHomepage } = props;
 
   const [info, setInfo] = useState<GetVideoResult>();
   const [player, setPlayer] = useState<YouTubePlayer>({});
@@ -42,13 +36,6 @@ const App2 = (props) => {
     };
   }, [])
 
-  // useEffect(()=>{
-  //   let j=setTimeout(() => {
-  //     if(player)player.playVideo();
-  //   }, 2000);
-  //   return ()=>clearTimeout(j)
-  // },[player])
-  
   useEffect(() => {
     console.log('slug', slug);
     if (slug?.trim()) {
@@ -66,13 +53,7 @@ const App2 = (props) => {
         handleVideoYoutubeId(data.data.parts[0].videoYoutubeId);
         handleCurrentPart(data.data.parts[0]);
         handleSeconds(data.data.parts[0].startSeconds);
-        //setBanner("setBannerBottom", data.data.banners[0]);
-        //setBanner("setBannerLeft", data.data.banners[1]);
-        //setBanner("setBannerRight", data.data.banners[2]);
         console.log(data);
-        // getFont(data.data.column7).then(x=>{
-        //   console.log(x)
-        // })
       });
     }
   }, []);
@@ -80,32 +61,14 @@ const App2 = (props) => {
   const reportChange = useCallback(
     (state, handle) => {
       if (handle === screen) {
-        //handleFull(state);
+        // To do
       }
     },
     [screen]
   );
   const divVideoWapperRef = useRef<HTMLDivElement>();
   const [isFull, setIsFull] = useState(false);
-  const [widthHeightIframe, setWidthHeightIframe] = useState<{
-    width: string | number;
-    height: string | number;
-  }>({
-    width: "100%",
-    height: "100%",
-  });
-  function resize() {
-    if (divVideoWapperRef.current) {
-      let parentWidth = divVideoWapperRef?.current?.offsetWidth;
-      let widthTemp = divVideoWapperRef?.current?.offsetHeight * 1.777777777777778;
-      let heightTemp = divVideoWapperRef?.current?.offsetHeight * 2;
-      console.log(parentWidth, widthTemp, heightTemp);
-      setWidthHeightIframe({
-        width: parentWidth < widthTemp ? parentWidth : widthTemp,
-        height: heightTemp,
-      });
-    }
-  }
+
   const handleFullScreen = () => {
     if (isFull) {
       setIsFull(false);
@@ -115,21 +78,11 @@ const App2 = (props) => {
       screen.enter();
     }
   };
-  useEffect(() => {
-    // window.addEventListener("resize", resize, false);
-    // return () => window.removeEventListener("resize", resize, false);
-  }, []);
-  //console.log(isReady);
+
   useEffect(() => {
     if (isReady) {
-      //console.log("READY HANDLE", seconds);
       player.playVideo();
       player.seekTo(seconds);
-      // setTimeout(() => {
-      //   let a = player.getPlayerState();
-      //   console.log(a);
-      //   if (a !== 1) player.playVideo();
-      // }, 1000);
     }
   }, [triggerPlay]);
 
@@ -165,45 +118,27 @@ const App2 = (props) => {
     }
   }
   const valueContext: IPlayer = {
-    mute,
-    pauseVideo,
-    playVideo,
-    seekTo,
-    stopVideo,
-    handleCurrentPart,
-    player,
-    nextPart,
-    info: info,
-    qualityChange: () => { },
-    setQuality: () => { }
+    mute, pauseVideo, playVideo, seekTo, stopVideo, handleCurrentPart,
+    player, nextPart, info: info, qualityChange: () => { }, setQuality: () => { }
   };
 
   const onReady = (e: YouTubeEvent) => {
-    //console.log("READY CALL");
     console.log(e);
     setPlayer(() => e.target);
-    // setTimeout(() => {
-    //   handleTriggerPlay();
-    // }, 2500);
   };
 
   const onStateChange = (gg: YouTubeEvent) => {
-    // console.log("CHANGE STATE CALL");
     let data = gg.data;
-    //console.log('state change: ',data);
     handleStateChange(data);
   };
 
   const onPlaybackQualityChange = (gg: YouTubeEvent) => {
-    // console.log("CHANGE STATE CALL");
     let data = gg.data;
     console.log('onPlaybackQualityChange: ', data);
-    //handleQualityChange(data);
   };
 
   const onEnd = () => {
-    //console.log("END OF VIDEO ", gg);
-    //nextPart();
+    // To do
   };
 
   return (
@@ -220,44 +155,26 @@ const App2 = (props) => {
             >
               {videoYoutubeId && (
                 <>
-                  <YouTube
-                    id="asdfas"
-                    videoId={videoYoutubeId}
-                    onReady={onReady}
-                    onPlaybackQualityChange={onPlaybackQualityChange}
-                    onStateChange={onStateChange}
-                    onEnd={onEnd}
-                    //   onReady={onReady}
-                    //   onPlaybackQualityChange={onPlaybackQualityChange}
-                    //   onStateChange={onStateChange}
-                    //   onEnd={onEnd}
-                    opts={{
-                      playsinline: 1,
-                      autoplay: 1,
-                      playerVars: {
-                        fs: 0,
-                        autoplay: 1,
-                        controls: 1,
+                  {videoYoutubeId.includes("http") ?
+                    <iframe src={videoYoutubeId} width="100%" height="100%" /> :
+                    <YouTube
+                      id="youtube-player"
+                      videoId={videoYoutubeId}
+                      onReady={onReady}
+                      onPlaybackQualityChange={onPlaybackQualityChange}
+                      onStateChange={onStateChange}
+                      onEnd={onEnd}
+                      opts={{
                         playsinline: 1,
-                        // rel: 0,
-                        // showinfo: 0,
-                        // mute: 0,
-                        // loop: 1,
-                      },
-                    }}
-                    iframeClassName="iframe2"
-                    title=""
-                    style={{ width: "100%", height: "100%" }}
-                  />
-                  <Box
-                    style={{
-                      background: "black",
-                      height: 38,
-                      position: "absolute",
-                      bottom: 0,
-                      right: 0,
-                    }}
-                  >
+                        autoplay: 1,
+                        playerVars: { fs: 0, autoplay: 1, controls: 1, playsinline: 1, },
+                      }}
+                      iframeClassName="iframe2"
+                      title=""
+                      style={{ width: "100%", height: "100%" }}
+                    />
+                  }
+                  <Box style={{ background: "black", height: 38, position: "absolute", bottom: 0, right: 0, }} >
                     {!isFull && (
                       <Button onClick={handleFullScreen} style={{ color: "#fff" }}>
                         <Expand />
